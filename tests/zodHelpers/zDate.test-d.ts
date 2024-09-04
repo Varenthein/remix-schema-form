ccf22d3741807f59084f730a65e42477493557fa
Paweł Zagrobelny <raventhein@gmail.com>
2024-09-04 17:33:16 +0200

    Test: Add additonal testes for zDate

--- tests/zodHelpers/zDate.test-d.ts
+++ tests/zodHelpers/zDate.test-d.ts
@@ -22,3 +22,80 @@ test("should return date type if valid data provided and required is true", () =
     test: Date;
   }>();
 });
+
+test("should return proper (date/undefined) type if valid data and minDate options provided (but with required = false)", () => {
+  const mySchema = z.object({
+    test: zDate({
+      minDate: new Date(),
+    }),
+  });
+
+  expectTypeOf<z.infer<typeof mySchema>>().toEqualTypeOf<{
+    test?: Date | undefined;
+  }>();
+});
+
+test("should return proper (date/undefined) type if valid data and maxDate options provided (but with required = false)", () => {
+  const mySchema = z.object({
+    test: zDate({
+      maxDate: new Date(),
+    }),
+  });
+
+  expectTypeOf<z.infer<typeof mySchema>>().toEqualTypeOf<{
+    test?: Date | undefined;
+  }>();
+});
+
+test("should return proper (date/undefined) type if valid data and minDate+maxDate options provided (but with required = false)", () => {
+  const mySchema = z.object({
+    test: zDate({
+      minDate: new Date(),
+      maxDate: new Date(),
+    }),
+  });
+
+  expectTypeOf<z.infer<typeof mySchema>>().toEqualTypeOf<{
+    test?: Date | undefined;
+  }>();
+});
+
+test("should return proper type if valid data and minDate options provided (required = true)", () => {
+  const mySchema = z.object({
+    test: zDate({
+      required: true,
+      minDate: new Date(),
+    }),
+  });
+
+  expectTypeOf<z.infer<typeof mySchema>>().toEqualTypeOf<{
+    test: Date;
+  }>();
+});
+
+test("should return proper type if valid data and maxDate options provided (required = true)", () => {
+  const mySchema = z.object({
+    test: zDate({
+      required: true,
+      maxDate: new Date(),
+    }),
+  });
+
+  expectTypeOf<z.infer<typeof mySchema>>().toEqualTypeOf<{
+    test: Date;
+  }>();
+});
+
+test("should return proper type if valid data and minDate+maxDate options provided (required = true)", () => {
+  const mySchema = z.object({
+    test: zDate({
+      required: true,
+      minDate: new Date(),
+      maxDate: new Date(),
+    }),
+  });
+
+  expectTypeOf<z.infer<typeof mySchema>>().toEqualTypeOf<{
+    test: Date;
+  }>();
+});
